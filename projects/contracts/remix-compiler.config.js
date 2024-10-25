b492c92d5a031af5b6a5100f400c1932344adaff
Steve Rodrigue <stev.rodr@gmail.com>
2024-10-25 13:53:51 -0400

    Began the LegitDAO Currency unit testing

--- projects/contracts/remix-compiler.config.js
+++ projects/contracts/remix-compiler.config.js
@@ -2,7 +2,7 @@ module.exports = {
               solidity: '0.8.26',
               settings: {
                 optimizer: {
-                  enabled: true,
+                  enabled: false,
                   runs: 200
                 }
               }
---
+++ projects/contracts/tests/LegitDAOCurrency_test.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: GPL-3.0
+        
+pragma solidity >=0.4.22 <0.9.0;
+
+// This import is automatically injected by Remix
+import "remix_tests.sol"; 
+
+// This import is required to use custom transaction context
+// Although it may fail compilation in 'Solidity Compiler' plugin
+// But it will work fine in 'Solidity Unit Testing' plugin
+import "remix_accounts.sol";
+import "../erc-20/LegitDAOCurrency.sol";
+
+// File name has to end with '_test.sol', this file can contain more than one testSuite contracts
+contract testSuite {
+    uint256 initialPricePerBNB;
+    uint256 priceIncrement;
+    uint256 buyTaxPercentage;
+    uint256 sellTaxPercentage;
+
+    LegitDAOCurrency instance;
+
+    /// 'beforeAll' runs before all other tests
+    /// More special functions are: 'beforeEach', 'beforeAll', 'afterEach' & 'afterAll'
+    function beforeAll() public {
+        initialPricePerBNB = 100;
+        priceIncrement = 10;
+        buyTaxPercentage = 5;
+        sellTaxPercentage = 5;
+        instance = new LegitDAOCurrency(
+            initialPricePerBNB,
+            priceIncrement,
+            buyTaxPercentage,
+            sellTaxPercentage
+        );
+
+        Assert.equal(uint(1), uint(1), "1 should be equal to 1");
+    }
+
+    function checkPublicPropertiesSuccess() public {
+       Assert.equal(instance.initialPricePerBNB(), initialPricePerBNB, "Initial price per BNB is invalid");
+       Assert.equal(instance.priceIncrement(), priceIncrement, "Price increment is invalid");
+       Assert.equal(instance.buyTaxPercentage(), buyTaxPercentage, "Buy tax percentage is invalid");
+       Assert.equal(instance.sellTaxPercentage(), sellTaxPercentage, "Sell tax percentage is invalid");
+    }
+
+    function checkSuccess() public {
+        // Use 'Assert' methods: https://remix-ide.readthedocs.io/en/latest/assert_library.html
+        Assert.ok(2 == 2, 'should be true');
+        Assert.greaterThan(uint(2), uint(1), "2 should be greater than to 1");
+        Assert.lesserThan(uint(2), uint(3), "2 should be lesser than to 3");
+    }
+
+    function checkSuccess2() public pure returns (bool) {
+        // Use the return value (true or false) to test the contract
+        return true;
+    }
+
+    /// Custom Transaction Context: https://remix-ide.readthedocs.io/en/latest/unittesting.html#customization
+    /// #sender: account-1
+    /// #value: 100
+    function checkSenderAndValue() public payable {
+        // account index varies 0-9, value is in wei
+        Assert.equal(msg.sender, TestsAccounts.getAccount(1), "Invalid sender");
+        Assert.equal(msg.value, 100, "Invalid value");
+    }
+}
+    
