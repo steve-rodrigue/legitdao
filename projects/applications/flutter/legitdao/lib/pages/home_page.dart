6fe725fbaf549ad5a45e82013d39b7406a6ad56e
Steve Rodrigue <stev.rodr@gmail.com>
2024-12-02 11:13:39 -0500

    Added the paragraph widget

--- projects/applications/flutter/legitdao/lib/pages/home_page.dart
+++ projects/applications/flutter/legitdao/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/visuals/header.dart';
+import '../widgets/visuals/paragraph.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -14,18 +15,8 @@ class HomePage extends StatelessWidget {
             value: 'home_title',
             isLarge: true,
           ),
-          Padding(
-            padding: EdgeInsets.all(5.0),
-            child: const Text(
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer pellentesque tellus ac libero euismod, at maximus justo cursus. Nulla a euismod nunc, sit amet tincidunt ante. Etiam commodo, risus ut sagittis luctus, mauris purus lobortis neque, ut molestie mauris purus a dui. Vestibulum vulputate lorem ut libero laoreet auctor. Integer in euismod purus, nec interdum sapien. Donec sed commodo turpis. Quisque condimentum vulputate mattis. Cras a enim et justo fermentum ornare tempus eu augue. Integer augue eros, fringilla sit amet euismod sed, dictum in arcu.",
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(5.0),
-            child: const Text(
-              "Mauris ac egestas est. Aenean pulvinar tortor vitae nunc vehicula cursus. Ut ut euismod purus. Curabitur tempor magna vitae dui dignissim mattis vel id nisl. Nunc pellentesque justo a eros laoreet, in finibus justo malesuada. Vivamus at volutpat ante. Donec pulvinar fermentum sapien, ut congue urna viverra vel. Aliquam vel pellentesque lorem. Aliquam erat volutpat. Vestibulum et neque ex. Cras pretium, nisi nec ultricies sollicitudin, risus orci aliquam erat, a interdum enim risus vel nulla. Nulla nec lectus sed nulla convallis fermentum. In cursus sapien et lacus venenatis, nec viverra purus commodo. Sed non congue dolor. Duis id nulla sit amet felis commodo rutrum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. ",
-            ),
-          ),
+          Paragraph(value: 'home_first_paragraph', isLarge: true),
+          Paragraph(value: 'home_second_paragraph', isLarge: true),
         ],
       ),
     );
---
+++ projects/applications/flutter/legitdao/lib/widgets/visuals/paragraph.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+class Paragraph extends StatelessWidget {
+  final bool isLarge;
+  final bool isMedium;
+  final bool isSmall;
+  final String value;
+
+  const Paragraph({
+    super.key,
+    required this.value,
+    this.isLarge = false,
+    this.isMedium = false,
+    this.isSmall = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    TextStyle textStyle = const TextStyle(fontWeight: FontWeight.bold);
+
+    if (isLarge && theme.textTheme.bodyLarge != null) {
+      textStyle = textStyle.merge(theme.textTheme.bodyLarge);
+    }
+
+    if (isMedium && theme.textTheme.bodyMedium != null) {
+      textStyle = textStyle.merge(theme.textTheme.bodyMedium);
+    }
+
+    if (isSmall && theme.textTheme.bodySmall != null) {
+      textStyle = textStyle.merge(theme.textTheme.bodySmall);
+    }
+
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
+      child: Text(
+        value.tr(),
+        style: textStyle,
+      ),
+    );
+  }
+}
